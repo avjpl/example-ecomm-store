f2681f3f35946fb9bc743c817b3ee754f380fc7a
Adrian Lawrence <adrian.lawrence@sky.uk>
2022-12-03 14:23:48 +0000

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+import products from '../../products.json';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Space Jeely Tshirt</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Space Jelly Shop
+        </h1>
+
+        <p className={styles.description}>
+          The best space jellyfish swag on the web!
+        </p>
+
+        <ul className={styles.grid}>
+            {
+                products.map(({id, title, description, image, price}) => (
+                    <li key={id} className={styles.card}>
+                        <a href="https://nextjs.org/docs">
+                            <img src={image} alt={title} />
+                            <h2>Space Jelly Tshirt</h2>
+                            <p>£{price}</p>
+                            <p>{description}</p>
+                        </a>
+                    </li>
+                ))
+            }
+        </ul>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
