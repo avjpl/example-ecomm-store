78aac1cfb1b16706fae174ab8348e1c31ee9fb90
Adrian Lawrence <adrian.lawrence@sky.uk>
2022-12-03 15:45:53 +0000

    relocated lib folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
-import { initiateCheckout } from './lib/payments';
+import { initiateCheckout } from '../lib/payments';
 
 import products from '../../products.json';
 
